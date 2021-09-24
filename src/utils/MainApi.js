8d95c7c8647b1cc886a77a2d027272a6917ddfc0
vsmeyukha <vsmeyukha@gmail.com>
2021-09-24 17:22:36 +0300

    env окружение

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 async function _checkResponse(res) {
   const awaitRes = await res.json();
